fc0fc64f555e44b6693b1e2bfc029aba4b08cac7
soulayma <dhimenesalma@gmail.com>
2025-09-16 00:34:00 +0100

    created the custom class based context manager for Database connection

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        #initialize the object
+        self.db_name = db_name
+        self.connection = None
+    
+    def __enter__(self):
+        #open the conection
+        self.connection = sqlite3.connect(self.db_name)
+        return self.connection
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        #close the connection
+        if self.connection:
+            self.connection.close()
+
+if __name__ == "__main__":
+        with DatabaseConnection('user.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM users;")
+            # fetchall() returns a list of tuples, where each tuple represents a row from the table.
+            results = cursor.fetchall()
+            print(results)
