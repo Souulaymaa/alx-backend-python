622a5010aefdda2693dab2299e08a13f304d1a70
soulayma <dhimenesalma@gmail.com>
2025-09-18 12:49:11 +0100

    added test_utils to test the method access_nested_map

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+
+class TestAccessNestedMap(unittest.TestCase):
+
+    @parameterized.expand([
+        ({"a":1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a"), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+        
+    @parameterized.expand([
+        ({}, ("a"), KeyError ),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self,nested_map, path, exception):
+        #using assertRaises as a context manager
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
