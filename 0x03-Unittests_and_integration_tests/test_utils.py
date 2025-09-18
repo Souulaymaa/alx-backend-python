32cdf8ecd7f151d6ab3eb1a07f772e6cf382cec1
soulayma <dhimenesalma@gmail.com>
2025-09-18 15:55:33 +0100

    added documentation

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# Unit Tests and Integration Tests
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
@@ -16,7 +17,7 @@ class TestAccessNestedMap(unittest.TestCase):
     # testing if the function returns the expected result.
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
-    
+
     @parameterized.expand([
         ({}, ("a"), KeyError),
         ({"a": 1}, ("a", "b"), KeyError)
