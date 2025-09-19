f6ec81d9920eff25dda6f9f728239c5633cddc70
soulayma <dhimenesalma@gmail.com>
2025-09-19 13:18:27 +0100

    added test_public_repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -9,7 +9,7 @@ Tests:
 
 import unittest
 from client import GithubOrgClient
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -21,7 +21,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ('abc',)
     ])
     @patch("client.get_json")
-    def test_org(self,orgname, mock_getjson):
+    def test_org(self, orgname, mock_getjson:Mock):
         '''
         Test that GithubOrgClient.org returns the correct value
         '''
@@ -33,4 +33,14 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_getjson.assert_called_once_with(
             f"https://api.github.com/orgs/{orgname}"
         )
+
+
+    def test_public_repos_url(self):
+        '''
+        Test if _public_repos_url returns expected URL
+        '''
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {"repos_url": "https://api.github.com/orgs/example/repos"}
+            client = GithubOrgClient('example')
+            self.assertEqual(client._public_repos_url, "https://api.github.com/orgs/example/repos")
 
