ac5ca755be4a4abec64617efea134541b2e7ef29
soulayma <dhimenesalma@gmail.com>
2025-09-19 14:05:23 +0100

    last last modif

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -53,7 +53,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_public_repos(self, mock_getjson: Mock):
         '''
-        Test that GithubOrgClient.public_repos 
+        Test that GithubOrgClient.public_repos
         returns the expected list of repos
         '''
         # fake payload (what get_json would normally return)
