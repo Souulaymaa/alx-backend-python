245ebd0f2e061d7b2024c19711e7e53a17d081e5
soulayma <dhimenesalma@gmail.com>
2025-09-21 15:52:41 +0100

    fixed the nested relationships issue

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -7,13 +7,10 @@ from .models import User, Conversation, Message
 # User Serializer
 # ------------------------
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        # Exclude password from read operations
         fields = ['user_id', 'first_name', 'last_name', 'email', 'phone_number', 'role', 'created_at', 'password']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
 
     def create(self, validated_data):
         # Use set_password to hash password
@@ -28,19 +25,24 @@ class UserSerializer(serializers.ModelSerializer):
 # Message Serializer
 # ------------------------
 class MessageSerializer(serializers.ModelSerializer):
-    sender = UserSerializer(read_only=True)
+    sender_name = serializers.CharField(source='sender.email', read_only=True)
 
     class Meta:
         model = Message
-        fields = ['message_id', 'sender', 'message_body', 'sent_at']
+        fields = ['message_id', 'sender', 'sender_name', 'message_body', 'sent_at']
 
 # ------------------------
 # Conversation Serializer
 # ------------------------
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
-    messages = MessageSerializer(many=True, read_only=True)
+    messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants', 'messages', 'created_at']
+
+    def get_messages(self, obj):
+        # Return serialized messages for this conversation
+        messages = obj.messages.all()
+        return MessageSerializer(messages, many=True).data
