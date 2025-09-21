a195f9131e429767a0b3d91daa2c569cd0353b0e
soulayma <dhimenesalma@gmail.com>
2025-09-21 13:41:29 +0100

    try

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -116,7 +116,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     def setUpClass(cls):
         '''Start patcher for requests.get'''
         cls.get_patcher = patch('requests.get')
-        mock_get = cls.get_patcher.start()
+        cls.mock_get = cls.get_patcher.start()
 
         def side_effect(url):
             if url == GithubOrgClient.ORG_URL.format('google'):
@@ -125,7 +125,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
                 return MockResponse(cls.repos_payload)
             return None
         
-        mock_get.side_effect = side_effect
+        cls.mock_get.side_effect = side_effect
 
     @classmethod
     def tearDownClass(cls):
